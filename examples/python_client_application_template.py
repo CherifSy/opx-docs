eb21297e79241633117cf59a6d39b28be0f1a1d3
Mike Lazar <mihai_lazar@dell.com>
2017-09-21 13:15:56 -0400

    Update python_client_application_template.py

--- examples/python_client_application_template.py
+++ examples/python_client_application_template.py
@@ -1,7 +1,11 @@
 import   cps
 import   cps_utils
 
-#  Example   GET  request cps_get_response   =  []
+def cps_attr_get(cps_data, attr):
+        return cps_utils.cps_attr_types_map.from_data(attr, cps_data[attr])
+
+#  Example   GET  request 
+cps_get_response   =  []
 cps.get([cps.key_from_name('observed','base-pas/chassis')], cps_get_response) 
 
 chassis_vendor_name   =  cps_attr_get(cps_get_response[0]['data'],'base-pas/chassis/vendor-name')
